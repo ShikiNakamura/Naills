fd4b0880a2e2c9267706cedd9ea284e7aebfe7b3
WIN10K310 <frau.lasckova2015@yandex.ru>
2024-03-25 17:57:22 +0700

    nails

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,9 +4,7 @@ function Header() {
   return (
     <header className="header">
       <div className="header_container">
-        
-				<div className="header_row">
-
+        <div className="header_row">
           <div className="name">
             <a className="label" href="#">
               <h2>THE NAIL STUDIO</h2>
@@ -17,17 +15,32 @@ function Header() {
             <a className="home" href="#">
               <p>Home</p>
             </a>
-						<a className="services" href="#">
+            <a className="services" href="#">
               <p>Services</p>
             </a>
           </div>
-
-        
-            
-         
-
         </div>
 
+        <div className="slogan">
+          <div className="welcome">
+            <p>WELCOME TO THE NAIL STUDIO</p>
+          </div>
+
+          <div className="beauty">
+            <p>BEAUTY</p>
+          </div>
+
+          <div className="start">
+            <p>STARTS WITH NAILS</p>
+          </div>
+
+          <div className="moto">
+            <p>
+              "we believe that every manicure and pedicure should be an
+              experience to remember"
+            </p>
+          </div>
+        </div>
       </div>
     </header>
   );
